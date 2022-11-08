d246460545e9305a6a8011786930332f72fdeaf8
Shaekh Hasan Shoron <shaekh.shoron@klovercloud.com>
2022-11-08 17:13:39 +0600

    hello

--- src/calculator_test.go
+++ src/calculator_test.go
@@ -16,9 +16,10 @@ func TestMinus(t *testing.T) {
 	}
 }
 
-func TestMinus2(t *testing.T) {
-	total := Minus(5, 5)
-	if total != 5 {
-		t.Errorf("Minus was incorrect, got: %d, want: %d.", total, 5)
-	}
-}
+//
+//func TestMinus2(t *testing.T) {
+//	total := Minus(5, 5)
+//	if total != 5 {
+//		t.Errorf("Minus was incorrect, got: %d, want: %d.", total, 5)
+//	}
+//}
